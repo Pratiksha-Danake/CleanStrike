eb6bf6b10d1d8acedb3755ac4bb847bafcb14599
Danake Pratiksha <danakepratiksha1020@gmail.com>
2024-04-11 06:37:01 +0530

    added test cases to create carrom board functionality at repository level

---
+++ CleanStrike/src/test/java/com/amaap/cleanstrike/repository/impl/InMemoryCarromBoardRepositoryTest.java
@@ -0,0 +1,31 @@
+package com.amaap.cleanstrike.repository.impl;
+
+import com.amaap.cleanstrike.domain.model.CarromBoard;
+import com.amaap.cleanstrike.repository.CarromBoardRepository;
+import com.amaap.cleanstrike.repository.db.InMemoryDatabase;
+import com.amaap.cleanstrike.repository.db.impl.FakeInMemoryDatabase;
+import com.amaap.cleanstrike.service.CarromBoardService;
+import com.amaap.cleanstrike.service.exception.InvalidCarromCoinsException;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class InMemoryCarromBoardRepositoryTest {
+    InMemoryDatabase inMemoryDatabase = new FakeInMemoryDatabase();
+    CarromBoardRepository carromBoardRepository = new InMemoryCarromBoardRepository(inMemoryDatabase);
+    CarromBoardService carromBoardService = new CarromBoardService(carromBoardRepository);
+
+    @Test
+    void shouldBeAbleToAddCreatedCarromBoardToDatabase() throws InvalidCarromCoinsException {
+        // arrange
+        int noOfBlackCoins = 9;
+        int noOfRedCoins = 1;
+        CarromBoard expected = carromBoardService.createCarromBoard(noOfBlackCoins, noOfRedCoins);
+
+        // act
+        CarromBoard actual = carromBoardRepository.addCarromBoard(expected);
+
+        // assert
+        assertEquals(expected, actual);
+    }
+}
